36a6eee24422254da1a7a9757939c50a2c5c77e8
Daniel Kempner <dkempner@lyft.com>
2021-12-01 21:08:18 -0800

    day 2 solved

--- day02/1.ts
+++ day02/1.ts
@@ -1,3 +1,17 @@
 import { solve } from './helper'
 
-solve((input) => {})
+solve((input) => {
+  let x = 0
+  let y = 0
+  input.forEach((i) => {
+    const split = i.split(' ')
+    const direction = split[0]
+    const num = Number(split[1])
+
+    if (direction === 'down') y += num
+    if (direction === 'up') y -= num
+    if (direction === 'back') x -= num
+    if (direction === 'forward') x += num
+  })
+  return x * y
+})
--- day02/2.ts
+++ day02/2.ts
@@ -1,3 +1,20 @@
 import { solve } from './helper'
 
-solve((input) => {})
+solve((input) => {
+  let x = 0
+  let y = 0
+  let aim = 0
+  input.forEach((i) => {
+    const split = i.split(' ')
+    const direction = split[0]
+    const num = Number(split[1])
+
+    if (direction === 'down') aim += num
+    if (direction === 'up') aim -= num
+    if (direction === 'forward') {
+      x = x + num
+      y = y + aim * num
+    }
+  })
+  return x * y
+})
