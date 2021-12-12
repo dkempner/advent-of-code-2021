f61f6fc0d460cf4517ebd2e34e24e7513151363f
Daniel Kempner <dkempner@lyft.com>
2021-12-12 10:04:17 -0800

    day07 p2

--- day07/2.ts
+++ day07/2.ts
@@ -1,7 +1,43 @@
 import { solve, bitsToInt } from './helper'
 
-solve((lines) => {
-  lines.forEach((line) => {
-    console.log(line)
+const penalty = (num: number) => {
+  let total = 0
+  for (let i = num; i > 0; i--) {
+    total = total + i
+  }
+  return total
+}
+
+const calcFuel = (mid: number, positions: number[]) => {
+  let total = 0
+  positions.forEach((p) => {
+    const absDiff = Math.abs(mid - p)
+    total = total + penalty(absDiff)
   })
+
+  return total
+}
+
+solve((lines) => {
+  let positions: number[] = []
+  lines.forEach((line) => {
+    line
+      .split(',')
+      .map(Number)
+      .sort((a, b) => a - b)
+      .forEach((n) => positions.push(n))
+  })
+
+  let min = Infinity
+  let minGuess = positions[0]
+  let maxGuess = positions[positions.length - 1]
+
+  for (let i = minGuess; i <= maxGuess; i++) {
+    let fuel = calcFuel(i, positions)
+    if (fuel < min) {
+      min = fuel
+    }
+  }
+
+  return min
 })
